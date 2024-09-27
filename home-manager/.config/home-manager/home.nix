055144507a1d0bbd5bdfb2542b46071220c7b6fc
Nathan Lee <nathan@globalphobia.com>
2024-09-27 12:41:55 -0600

    Update nix settings

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -13,7 +13,8 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "23.05"; # Please read the comment before changing.
+  # home.stateVersion = "23.05"; # Please read the comment before changing.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -52,6 +53,9 @@
     pkgs.lsd
     pkgs.mosh
     pkgs.tig
+    pkgs.ansible
+    pkgs.awscli
+    pkgs.mariadb
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
